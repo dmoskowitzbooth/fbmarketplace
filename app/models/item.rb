38247e3db7be19b90e164743f0bf50835828b434
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2025-05-10 05:58:57 +0000

    1

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id                :bigint           not null, primary key
+#  address           :string
+#  asking_price      :float
+#  category          :string
+#  description       :text
+#  image             :string
+#  msg_chains_count  :integer
+#  name              :string
+#  offers_count      :integer
+#  og_purchase_price :float
+#  reviews_count     :integer
+#  status            :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  seller_id         :integer
+#
+class Item < ApplicationRecord
+end
